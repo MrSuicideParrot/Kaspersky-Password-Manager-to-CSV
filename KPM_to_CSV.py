bfb2d3caf5d57f67905a9ffb6d532cd937593735
André Cirne <ancirne@gmail.com>
2017-06-10 21:24:18 +0100

    Parse do documento
    falta impressão

--- KPM_to_CSV.py
+++ KPM_to_CSV.py
@@ -1,59 +1,120 @@
 import argparse
 
-def websiteParser(iterador, fdw):
-    pala = next(iterador)
 
-    while pala != 'Applications':
-        str = 'Websites,'
+class Websites:
+    def __init__(self, name, url, login, password, account):
+        self.website = name
+        self.url = url
+        self.login = login
+        self.password = password
+        self.accountname = account
+
+    def prepare_print(self, delimiter):
+        return self.website + delimiter + self.url + delimiter + self.login + delimiter + self.password + self.accountname
+
+
+class Applications:
+    def __init__(self, app, login, password, account):
+        self.app = app
+        self.login = login
+        self.password = password
+        self.account = account
+
+    def prepare_print(self, delimiter):
+        return self.app + delimiter + self.login + delimiter + self.password + delimiter + self.account
+
+
+class Note:
+    def __init__(self, text):
+        self.text = text
+
+
+def main():
+    file = 'Karsp.txt'
+    websiteslist = []
+    applicationslist = []
+    notes = []
+
+    fd = open(file, 'r')
+    fd.readline()
+
+    while True:
+        args = []
+        fd.readline()
 
         '''Website name'''
-        str += pala[14:-1]
+        buf = fd.readline()
+        if buf == 'Applications\n':
+            break
 
-        pala = next(iterador)
+        args.append(buf[14:-1])
+
         '''Website URL'''
-        str += pala[13:-1]
+        buf = fd.readline()
+        args.append(buf[13:-1])
 
-        pala = next(iterador)
         '''Login'''
-        str += pala[7:-1]
+        buf = fd.readline()
+        args.append(buf[7:-1])
 
-        pala = next(iterador)
         '''Password'''
-        str += pala[10:-1]
+        buf = fd.readline()
+        args.append(buf[10:-1])
 
-        pala = next(iterador)
-        '''Acount'''
-        str += pala[14]
+        '''Account'''
+        buf = fd.readline()
+        args.append(buf[14:-1])
 
-        fdw.write(str)
+        websiteslist.append(Websites(*args))
 
-        next(iterador)
-        next(iterador)
-        next(iterador)
-        pala = next(iterador)
+        fd.readline()
+        fd.readline()
+
+    while True:
+        args = []
+        fd.readline()
+
+        '''aplication name'''
+        buf = fd.readline()
+
+        if buf == 'Notes\n':
+            break
+
+        args.append(buf[13:-1])
+
+        '''Login'''
+        buf = fd.readline()
+        args.append(buf[7:-1])
+
+        '''Password'''
+        buf = fd.readline()
+        args.append(buf[10:-1])
+
+        '''acount name'''
+        buf = fd.readline()
+        args.append(buf[14:-1])
+
+        applicationslist.append(Applications(*args))
+
+        fd.readline()
+        fd.readline()
+
+    while True:
+        args = ''
+        fd.readline()
+
+        '''aplication name'''
+        buf = fd.readline()
+
+        while buf != '---\n':
+            if buf is not '\n':
+                args += buf
+            buf = fd.readline()
+
+        notes.append(Note(args))
+
+    delimiter = ';'
 
 
-def appParser(iterador, fdw):
-    pala = next(iterador)
-
-    while pala != 'Notes':
-        str = 'Application,'
-
-
-file = 'teste.txt'
-
-output = 'password.csv'
-
-fdr = open(file, 'r')
-
-fdw = open(file, 'w')
-
-fileIter = fdr.readlines()
-
-line = next(fileIter)
-if line == 'Websites':
-    next(fileIter)
-    websiteParser(fileIter, fdw)
-    next(fileIter)
-    next(fileIter)
-    appParser(fileIter, fdw)
+if __name__ == '__main__':
+    main()
