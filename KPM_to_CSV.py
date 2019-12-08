901e8c58efea7ab1cd09d7235e85e32def73c32d
André Cirne <ancirne@gmail.com>
2019-12-08 14:37:05 +0000

    Script updated for the new export format

--- KPM_to_CSV.py
+++ KPM_to_CSV.py
@@ -1,28 +1,29 @@
-import argparse
 import codecs
 
 
 class Websites:
-    def __init__(self, name, url, login, password, account):
+    def __init__(self, name, url, login_name, login, password, account):
         self.website = name
         self.url = url
+        self.login_name = login_name
         self.login = login
         self.password = password
         self.accountname = account
 
     def prepare_print(self, delimiter):
-        return 'Websites'+delimiter+self.website + delimiter + self.url + delimiter + self.login + delimiter + self.password + self.accountname
+        return 'Websites'+delimiter+self.website + delimiter + self.url + delimiter + self.login_name + delimiter + self.login + delimiter + self.password + delimiter +self.accountname
 
 
 class Applications:
-    def __init__(self, app, login, password, account):
+    def __init__(self, app, login_name, login, password, comment):
         self.app = app
+        self.login_name = login_name
         self.login = login
         self.password = password
-        self.account = account
+        self.comment = comment
 
     def prepare_print(self, delimiter):
-        return 'Applications'+delimiter+self.app + delimiter + self.login + delimiter + self.password + delimiter + self.account
+        return 'Applications'+delimiter+self.app + delimiter +self.login_name + delimiter + self.login + delimiter + self.password + delimiter + self.comment
 
 
 class Note:
@@ -39,22 +40,30 @@ def main():
     change = False
     need_to_chamge = []
     fd = codecs.open(file, 'r', "utf-8")
-    fd.readline()
-
+    
     while True:
         args = []
+        
+        ''' Header '''
+        buf = fd.readline()
+        if buf == 'Applications\r\n':
+           break
+        
         fd.readline()
 
         '''Website name'''
         buf = fd.readline()
         if buf == 'Applications\r\n':
-            break
-
+           break
         args.append(buf[14:-2])
 
         '''Website URL'''
         buf = fd.readline()
         args.append(buf[13:-2])
+
+        '''Login name'''
+        buf = fd.readline()
+        args.append(buf[12:-2])
 
         '''Login'''
         buf = fd.readline()
@@ -64,9 +73,12 @@ def main():
         buf = fd.readline()
         args.append(buf[10:-2])
 
-        '''Account'''
+        '''Comment'''
         buf = fd.readline()
         args.append(buf[14:-2])
+
+        if args == ['', '', '', '', '', '']:
+            break
 
         websiteslist.append(Websites(*args))
 
@@ -76,7 +88,6 @@ def main():
                 change = True
                 break
 
-        fd.readline()
         fd.readline()
 
     while True:
@@ -91,6 +102,10 @@ def main():
 
         args.append(buf[13:-2])
 
+        '''Login name'''
+        buf = fd.readline()
+        args.append(buf[12:-2])
+
         '''Login'''
         buf = fd.readline()
         args.append(buf[7:-2])
@@ -99,7 +114,7 @@ def main():
         buf = fd.readline()
         args.append(buf[10:-2])
 
-        '''acount name'''
+        '''comment'''
         buf = fd.readline()
         args.append(buf[14:-2])
 
@@ -121,25 +136,25 @@ def main():
         '''notes name'''
         buf = fd.readline()
 
-        if buf is '':
+        if buf == '':
             break
 
         while buf != '---\r\n':
-            if buf is not '\r\n':
+            if buf != '\r\n':
                 args += buf
             buf = fd.readline()
 
     notes.append(Note(args))
 
     if change:
-        print("Necessita de mudar as seguintes contas!")
+        print("You need to change the following accounts:")
         for aux in need_to_chamge:
             print(aux.prepare_print(delimiter))
         return
 
     ''' csv '''
     '''Grupo'''
-    documento = 'passes_prontas.csv'
+    documento = 'converted_passwords.csv'
 
     fd = codecs.open(documento, 'wb', 'utf-8')
 
@@ -148,6 +163,8 @@ def main():
 
     for aux in applicationslist:
         fd.write((aux.prepare_print(delimiter)+'\n'))
+
+    print("Conversion finished - %d converted accounts" % (len(websiteslist)+len(applicationslist)))
 
 
 if __name__ == '__main__':
