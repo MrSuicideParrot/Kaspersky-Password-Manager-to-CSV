762bc86cf9aaa825686c45c07552d525960fc7b8
xzenx86 <pierre.dcm@live.fr>
2023-02-25 12:27:45 +0100

    Bug Fixes & improvements & Add nordpass converter option

--- KPM_to_CSV.py
+++ KPM_to_CSV.py
@@ -3,17 +3,20 @@ from argparse import ArgumentParser
 
 
 class Websites:
-    def __init__(self, name, url, login_name, login, password, account):
+    def __init__(self, name, url, login_name, login, password, comment):
         self.website = name
         self.url = url
         self.login_name = login_name
         self.login = login
         self.password = password
-        self.accountname = account
+        self.comment = comment
 
-    def prepare_print(self, delimiter):
-        return 'Websites'+delimiter+self.website + delimiter + self.url + delimiter + self.login_name + delimiter + self.login + delimiter + self.password + delimiter +self.accountname
-
+    def prepare_print(self, delimiter, nordpass=False):
+        if nordpass:
+            res = self.website + delimiter + self.url + delimiter + self.login + delimiter + "\"" + self.password + "\"" + delimiter + "\"" + self.comment + "\""
+        else:
+            res = 'Websites' + delimiter + self.website + delimiter + self.url + delimiter + self.login_name + delimiter + self.login + delimiter + "\"" + self.password + "\"" + delimiter + "\"" + self.comment + "\""
+        return res
 
 class Applications:
     def __init__(self, app, login_name, login, password, comment):
@@ -23,25 +26,38 @@ class Applications:
         self.password = password
         self.comment = comment
 
-    def prepare_print(self, delimiter):
-        return 'Applications'+delimiter+self.app + delimiter +self.login_name + delimiter + self.login + delimiter + self.password + delimiter + self.comment
-
+    def prepare_print(self, delimiter, nordpass=False):
+        if nordpass:
+            res = self.app + delimiter*2 + self.login + delimiter + "\"" + self.password + "\"" + delimiter + "\"" + self.comment + "\""
+        else:
+            res = 'Applications' + delimiter + self.app + delimiter +self.login_name + delimiter + self.login + delimiter + "\"" + self.password + "\"" + delimiter + "\"" + self.comment + "\""
+        return res
 
 class Note:
-    def __init__(self, text):
+    def __init__(self, name, text):
+        self.name = name
         self.text = text
+
+    def prepare_print(self, delimiter, nordpass=False):
+        if nordpass:
+            res = self.name + delimiter*4 + "\"" + self.text + "\""
+        else:
+            res = 'Note' + delimiter + self.name + delimiter + "\"" + self.text + "\""
+        return res
 
 
 def main():
-    
+
     argsParser = ArgumentParser(description='Kaspersky Password Manager conveter to CSV')
     argsParser.add_argument('-i', '--input_file', help='Kaspersky Password Manager export file')
     argsParser.add_argument('-o', '--output_file', default='converted_passwords.csv', type=str, help='Custom CSV file name')
+    argsParser.add_argument('-n', '--nordpass', default=False, type=bool, help='Convert to NordPass CSV')
 
     args = argsParser.parse_args()
 
     file = args.input_file
     documento = args.output_file
+    nordpass = args.nordpass
 
     if file == None:
         argsParser.print_help()
@@ -50,134 +66,112 @@ def main():
     delimiter = ';'
     websiteslist = []
     applicationslist = []
-    notes = []
-    change = False
-    need_to_chamge = []
+    noteslist = []
     fd = codecs.open(file, 'r', "utf-8")
-    
-    while True:
+    buf = fd.readline()
+
+    #Start by reading Websites attributes until Applications section
+    while buf !="Applications\n" :
         args = []
         
         ''' Header '''
         buf = fd.readline()
-        if buf == 'Applications\r\n':
-           break
-        
-        fd.readline()
+        if buf[:12] == "Website name":
+            args.append(buf[14:-1])
 
-        '''Website name'''
-        buf = fd.readline()
-        if buf == 'Applications\r\n' or  buf == 'Notes\r\n' or buf == '':
-           break
-        args.append(buf[14:-2])
-
-        '''Website URL'''
-        buf = fd.readline()
-        args.append(buf[13:-2])
-
-        '''Login name'''
-        buf = fd.readline()
-        args.append(buf[12:-2])
-
-        '''Login'''
-        buf = fd.readline()
-        args.append(buf[7:-2])
-
-        '''Password'''
-        buf = fd.readline()
-        args.append(buf[10:-2])
-
-        '''Comment'''
-        buf = fd.readline()
-        args.append(buf[14:-2])
-
-        if args == ['', '', '', '', '', '']:
-            break
-
-        websiteslist.append(Websites(*args))
-
-        for aux in args:
-            if delimiter in aux:
-                need_to_chamge.append(websiteslist[-1])
-                change = True
-                break
-
-        fd.readline()
-
-    while True:
-        args = []
-        fd.readline()
-
-        '''aplication name'''
-        buf = fd.readline()
-
-        if buf == 'Notes\r\n' or buf == '':
-            break
-
-        args.append(buf[13:-2])
-
-        '''Login name'''
-        buf = fd.readline()
-        args.append(buf[12:-2])
-
-        '''Login'''
-        buf = fd.readline()
-        args.append(buf[7:-2])
-
-        '''Password'''
-        buf = fd.readline()
-        args.append(buf[10:-2])
-
-        '''comment'''
-        buf = fd.readline()
-        args.append(buf[14:-2])
-
-        applicationslist.append(Applications(*args))
-
-        for aux in args:
-            if delimiter in aux:
-                need_to_chamge.append(applicationslist[-1])
-                change = True
-                break
-
-        fd.readline()
-        fd.readline()
-
-    while True:
-        args = ''
-        fd.readline()
-
-        '''notes name'''
-        buf = fd.readline()
-
-        if buf == '':
-            break
-
-        while buf != '---\r\n':
-            if buf != '\r\n':
-                args += buf
+            '''Website URL'''
             buf = fd.readline()
+            args.append(buf[13:-1])
 
-    notes.append(Note(args))
+            '''Login name'''
+            buf = fd.readline()
+            args.append(buf[12:-1])
 
-    if change:
-        print("You need to change the following accounts:")
-        for aux in need_to_chamge:
-            print(aux.prepare_print(delimiter))
-        return
+            '''Login'''
+            buf = fd.readline()
+            args.append(buf[7:-1])
+
+            '''Password'''
+            buf = fd.readline()
+            args.append(buf[10:-1])
+
+            '''Comment'''
+            buf = fd.readline()
+            buf = buf[:-1]
+            buf_tmp = fd.readline()
+            while buf_tmp != '\n':
+                buf += ' \\r ' + buf_tmp[:-1]
+                buf_tmp = fd.readline()
+            args.append(buf[9:])
+
+            websiteslist.append(Websites(*args))
+
+    while buf!="Notes\n" :
+        args = []
+        
+        ''' Header '''
+        buf = fd.readline()
+        if buf[:11] == "Application" :
+            args.append(buf[13:-1])
+
+            '''Login name'''
+            buf = fd.readline()
+            args.append(buf[12:-1])
+
+            '''Login'''
+            buf = fd.readline()
+            args.append(buf[7:-1])
+
+            '''Password'''
+            buf = fd.readline()
+            args.append(buf[10:-1])
+
+            '''Comment'''
+            buf = fd.readline()
+            buf = buf[:-1]
+            buf_tmp = fd.readline()
+            while buf_tmp != '\n':
+                buf += ' \\r ' + buf_tmp[:-1]
+                buf_tmp = fd.readline()
+            args.append(buf[9:])
+
+            applicationslist.append(Applications(*args))
+        
+    while buf :
+        args = []
+
+        buf = fd.readline()
+        if buf[:4] == "Name" :
+            args.append(buf[6:-1])
+
+            '''Text'''
+            buf = fd.readline()
+            buf = buf[:-1]
+            buf_tmp = fd.readline()
+            while buf_tmp != '\n':
+                buf += ' \\r ' + buf_tmp[:-1]
+                buf_tmp = fd.readline()
+            args.append(buf[6:])
+    
+            noteslist.append(Note(*args))
 
     ''' csv '''
     '''Grupo'''
 
     fd = codecs.open(documento, 'wb', 'utf-8')
 
+    if nordpass:
+        fd.write("name;url;username;password;note;cardholdername;cardnumber;cvc;expirydate;zipcode;folder;full_name;phone_number;email;address1;address2;city;country;state\n")
     for aux in websiteslist:
-        fd.write((aux.prepare_print(delimiter)+'\n'))
+        fd.write((aux.prepare_print(delimiter, nordpass)+'\n'))
 
     for aux in applicationslist:
-        fd.write((aux.prepare_print(delimiter)+'\n'))
+        fd.write((aux.prepare_print(delimiter, nordpass)+'\n'))
 
-    print("Conversion finished - %d converted accounts" % (len(websiteslist)+len(applicationslist)))
+    for aux in noteslist:
+        fd.write((aux.prepare_print(delimiter, nordpass)+'\n'))
+
+    print("Conversion finished - %d converted accounts" % (len(websiteslist)+len(applicationslist)+len(noteslist)))
 
 
 if __name__ == '__main__':
